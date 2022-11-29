b0fd0a2e4ce100528ec418787e8963ab6512206a
Siddharth Tiwari <tiwarisiddharth27@gmail.com>
2022-11-29 19:52:26 +0530

    undefined vs not defined

---
+++ undefined&notDefined.js
@@ -0,0 +1,13 @@
+//not defined and undefined in js
+console.log(a);
+if(a===undefined){
+    console.log("undefined");
+} else {
+    console.log("Not defined");
+}
+var a= "Rajat";
+console.log(a);
+// undefined = value is "undefined" yet but there is variable
+
+console.log(b)
+//not defined = b is "not defined", because there is no variable in memory
